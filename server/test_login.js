85e70d280a37ecdeefd32900f391bf77207e551e
Rethanya <rethanyav06@gmail.com>
2026-03-16 19:57:19 +0530

    Updated API base URL to production

--- server/test_login.js
+++ server/test_login.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const testLogin = async () => {
     try {
-        const response = await axios.post('http://localhost:5000/api/login', {
+        const response = await axios.post('https://bookmyturff.onrender.com/api/login', {
             email: 'admin@bookmyturf.com',
             password: 'adminpassword'
         });
--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://bookmyturff.onrender.com';
 
 export default API_BASE_URL;
